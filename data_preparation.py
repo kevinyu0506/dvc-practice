43e875669898757c97c7a5a6006f177531f92d91
kevinyu0506 <kevinyu05062006@gmail.com>
2020-11-26 16:44:19 +0800

    Separate data_preparation & train

---
+++ data_preparation.py
@@ -0,0 +1,79 @@
+'''This script goes along the blog post
+"Building powerful image classification models using very little data"
+from blog.keras.io.
+It uses data that can be downloaded at:
+https://www.kaggle.com/c/dogs-vs-cats/data
+In our setup, we:
+- created a data/ folder
+- created train/ and validation/ subfolders inside data/
+- created cats/ and dogs/ subfolders inside train/ and validation/
+- put the cat pictures index 0-999 in data/train/cats
+- put the cat pictures index 1000-1400 in data/validation/cats
+- put the dogs pictures index 12500-13499 in data/train/dogs
+- put the dog pictures index 13500-13900 in data/validation/dogs
+So that we have 1000 training examples for each class, and 400 validation examples for each class.
+In summary, this is our directory structure:
+```
+data/
+    train/
+        dogs/
+            dog001.jpg
+            dog002.jpg
+            ...
+        cats/
+            cat001.jpg
+            cat002.jpg
+            ...
+    validation/
+        dogs/
+            dog001.jpg
+            dog002.jpg
+            ...
+        cats/
+            cat001.jpg
+            cat002.jpg
+            ...
+```
+'''
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras import applications
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+nb_train_samples = 2000
+nb_validation_samples = 800
+batch_size = 10
+
+
+def save_bottlebeck_features():
+    datagen = ImageDataGenerator(rescale=1. / 255)
+
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
+
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_train = model.predict_generator(
+        generator, nb_train_samples // batch_size)
+    np.save('bottleneck_features_train.npy', bottleneck_features_train)
+
+    generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_validation = model.predict_generator(
+        generator, nb_validation_samples // batch_size)
+    np.save('bottleneck_features_validation.npy', bottleneck_features_validation)
+
+
+save_bottlebeck_features()
--- train.py
+++ train.py
@@ -36,50 +36,17 @@ data/
 ```
 '''
 import numpy as np
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dropout, Flatten, Dense
-from keras import applications
 from keras.callbacks import CSVLogger
 from tqdm.keras import TqdmCallback
 
-# dimensions of our images.
-img_width, img_height = 150, 150
 
 top_model_weights_path = 'model.h5'
-train_data_dir = 'data/train'
-validation_data_dir = 'data/validation'
 nb_train_samples = 2000
 nb_validation_samples = 800
 epochs = 10
 batch_size = 10
-
-
-def save_bottlebeck_features():
-    datagen = ImageDataGenerator(rescale=1. / 255)
-
-    # build the VGG16 network
-    model = applications.VGG16(include_top=False, weights='imagenet')
-
-    generator = datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)
-    bottleneck_features_train = model.predict_generator(
-        generator, nb_train_samples // batch_size)
-    np.save('bottleneck_features_train.npy', bottleneck_features_train)
-
-    generator = datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)
-    bottleneck_features_validation = model.predict_generator(
-        generator, nb_validation_samples // batch_size)
-    np.save('bottleneck_features_validation.npy', bottleneck_features_validation)
 
 
 def train_top_model():
@@ -109,5 +76,4 @@ def train_top_model():
     model.save_weights(top_model_weights_path)
 
 
-save_bottlebeck_features()
 train_top_model()
